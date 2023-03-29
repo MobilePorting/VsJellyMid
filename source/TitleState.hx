21acfe1dd154730e4dfaee4dd9bceef2b9b2d6e2
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-03-29 14:38:48 +0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -52,6 +52,8 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
+                #if android FlxG.android.preventDefaultKeys = [BACK]; #end
+
 		#if desktop
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
